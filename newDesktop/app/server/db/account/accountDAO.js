4057090dc0faf5ed85eaf44198ca313bfc61aa6f
jyotiPaxcel <jyoti.sharma@paxcel.net>
2015-01-22 17:48:48 +0530

    Added database commands

--- newDesktop/app/server/db/account/accountDAO.js
+++ newDesktop/app/server/db/account/accountDAO.js
@@ -1,8 +1,42 @@
 //Insert Query
-var insertPaymentSummaryQuery = "INSERT OR REPLACE INTO payment_sheet_summary(settlement_ref_id, order_id, external_id, settlement_date, order_item_id, order_status, sku, description, quantity, invoice_id, marketPlace, order_city, order_state, order_postal, invoice_amount,  shopping_credits, promotional_rebate, sales_tax, selling_fee, fba_fee, other_transaction_fee, other, total, settlement_value, order_item_value, refund, hold, reformance_award, protection_fund, total_marketplace_fee, comission_rate, commission_fee, fixed_fee, emi_fee, shipping_fee, reverse_shipping_fee, cancellation_fee, fee_discount, service_tax, dispatch_date, delivery_date, cancellation_date, dispute_date) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+var insertPaymentSummaryQuery = "INSERT OR REPLACE INTO payment_sheet_summary(settlement_ref_id, order_id, external_id, settlement_date, order_item_id, order_status, sku, description, quantity, invoice_id, marketPlace, order_city, order_state, order_postal, invoice_amount,  shipping_credits, promotional_rebate, sales_tax, selling_fee, fba_fee, other_transaction_fee, other, total, settlement_value, order_item_value, refund, hold, performance_award, protection_fund, total_marketplace_fee, comission_rate, commission_fee, fixed_fee, emi_fee, shipping_fee, reverse_shipping_fee, cancellation_fee, fee_discount, service_tax, dispatch_date, delivery_date, cancellation_date, dispute_date) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
 
-var insertAcctgTransQuery = "INSERT OR REPLACE INTO orders(orders_external_id, orders_channel_id, orders_order_date, orders_status_id, orders_external_status_id, orders_grand_total, orders_created_timestamp, orders_last_updated_timestamp) VALUES (?,?,?,?,?,?,?,?)";
+var insertAcctgTransQuery = "INSERT OR REPLACE INTO acctg_trans(acctg_trans_id, acctg_trans_type_id, description, transaction_date, is_posted,voucher_ref,voucher_date,order_id, inventory_item_id, party_id) VALUES (?,?,?,?,?,?,?,?,?,?)";
 
-var insertAcctgTransEntryQuery = "INSERT OR REPLACE INTO orders(orders_external_id, orders_channel_id, orders_order_date, orders_status_id, orders_external_status_id, orders_grand_total, orders_created_timestamp, orders_last_updated_timestamp) VALUES (?,?,?,?,?,?,?,?)";
+var insertAcctgTransEntryQuery = "INSERT OR REPLACE INTO acctg_trans_entry(acctg_trans_id, acctg_trans_entry_seq_id, acctg_trans_entry_type_id, voucher_ref, party_id, role_type_id, gl_account_type_id, gl_account_id,organization_party_id,amount,currency_uom_id,debit_credit_flag,reconcile_status_id,gl_account_class) VALUES (?,?,?,?,?,?,?,?)";
 
-//Select Query
+var refExistsCount = "select COUNT(*) as alreadyExist, from payment_sheet_summary where SETTLEMENT_REF_ID = ?";
+
+var importAmazonPaymentSheet = exports.importAmazonPaymentSheet = function (record) {
+    db.beginTransaction(function (err, transaction) {
+	transaction.run(insertPaymentSummaryQuery, [record[1], null,record[3], record[0], null, record[2], record[4], record[5], 			record[6],null,record[7], record[9],record[10],record[11],record[12], record[13], record[14],record[15],record[16],
+		record[17],record[18],record[19],record[20]]);
+        transaction.commit(function (err) {
+            if (err) {
+                console.error(err);
+                transaction.rollback();
+            }
+        });
+    });
+
+}
+
+var importFlipkartPaymentSheet = exports.importFlipkartPaymentSheet = function (record) {
+    db.beginTransaction(function (err, transaction) {
+	transaction.run(insertPaymentSummaryQuery, [record[0], null,record[2], record[1], record[3], record[5], record[6],null, 		record[7],record[8],null,null,null,null,record[10],null,null,null,null,null,null,null,null, record[11],null,record[13],
+		record[14],record[15],record[16],record[17],record[18],record[19],record[20],record[21],record[22],record[26],record[27],
+		record[28],record[29],record[30],record[31],record[32],record[33],record[34]]);
+        transaction.commit(function (err) {
+            if (err) {
+                console.error(err);
+                transaction.rollback();
+            }
+        });
+    });
+
+}
+
+var refAlreadyExists = exports.refAlreadyExists = function (refId) {
+	db.all(refExistsCount, refId);
+}
+
--- newDesktop/app/server/services/account/accountService.js
+++ newDesktop/app/server/services/account/accountService.js
@@ -19,59 +19,72 @@ function storePaymentSheetData(req,res) {
 console.log('in fucntion');
 	  var form = new formidable.IncomingForm();
 	  form.parse(req, function(err, fields, files) {
-	  res.writeHead(200, {'content-type': 'text/plain'});
-	  res.write('Received upload:\n\n');
-	  res.end('kulwanignh');
 		//  excel.excel(util.inspect(files['upload']['path']));
+	 	 if(fields['format']=='csv'){
+			fs.copy(files['upload']['path'], '../../..TempUploaded/file.csv', { replace: true },
+		 		function (err) {
+		        		if (err) {
+		           			// i.e. file already exists or can't write to directory
+						throw err;
+				} else {
+					csvParser.csvParse('../../..TempUploaded/file.csv');
+					fs.remove('../../..TempUploaded/file.csv');  //this line should come in each of parsing file .
+			          }
 	  
-	  if(fields['format']=='csv'){
-		fs.copy(files['upload']['path'], '../../..TempUploaded/file.csv', { replace: false },
-         		function (err) 
-				{
-                if (err) {
-                   // i.e. file already exists or can't write to directory
-						throw err;}
-           	    else {
-				console.log('csv parser clll');
-	                //  csvParser.csvParse('../../..TempUploaded/file.csv');
-	                 // fs.remove('../../..TempUploaded/file.csv');  //this line should come in each of parsing file .
-	                  }
-  
-			    });
-		//	 
-	     }
-			
-	  else if(fields['format']=='xlsx'){
-		fs.copy(files['upload']['path'], '../../..TempUploaded/file.xlsx', { replace: false },
-        		function (err) 
-				{
-                  if (err) {
-                    // i.e. file already exists or can't write to directory
-                        throw err;}
-	             else  
-				   {
-                      console.log('csv parser clll');				   
-				//       excelParser.excelParse('../../..TempUploaded/file.xlsx');
-                 //      fs.remove('../../..TempUploaded/file.xlsx'); //this line should come in each of parsing file .
-                   }
+			});
+	     	} else if(fields['format']=='xlsx'){
+			fs.copy(files['upload']['path'], '../../..TempUploaded/file.xlsx', { replace: false },
+        		function (err) {
+                  		if (err) {
+				    // i.e. file already exists or can't write to directory
+				        throw err;
+				} else {
+					excelParser.excelParse('../../..TempUploaded/file.xlsx');
+                 			fs.remove('../../..TempUploaded/file.xlsx'); //this line should come in each of parsing file .
+                   		}
 			 });
 		}
-			
-		
-	    });
-	
-
+	});
+	res.writeHead(200, {'content-type': 'text/plain'});
+	res.write('Received upload:\n\n');
+	res.end('kulwanignh');
 }
 
-function storeCSVData(filePath, response) {
-	//Implement FastCSV for Csv parsing
-	//Call queries from accountDAO, SAve
-}
-function storexlsxData(filePath, response) {
-	//Implement excel(dependency on visualstudio) for Csv parsing
-	//Call queries from accountDAO, SAve
+function createFlipkartPaymentSheet() {
+	csv
+	 .fromStream(stream)
+	 .on("data", function(data){
+	     db.serialize(function() {
+		if(counter > 0) {
+			//Check sheet's column count, sequence and name
+			//Check if settlementId, date time, type is not empty
+			//Check if settlementId exists in PaymentSummary entity
+			var existingCount = refAlreadyExists(refId);
+		}counter++;
+	     });
+	 })
+	 .on("end", function(){
+	     console.log("done");
+	 });
 }
 
+function createAmazonPaymentSheet() {
+	csv
+	 .fromStream(stream)
+	 .on("data", function(data){
+	     db.serialize(function() {
+		if(counter > 0) {
+			//Check sheet's column count, sequence and name
+			//Check if settlementId, date time, type is not empty
+			//Check if settlementId exists in PaymentSummary entity
+			var existingCount = refAlreadyExists(refId);
+		}counter++;
+	     });
+	 })
+	 .on("end", function(){
+	     console.log("done");
+	 });
+}
 exports.storePaymentSheetData=storePaymentSheetData;
 exports.storeCSVData=storeCSVData;
 exports.storexlsxData=storexlsxData;
